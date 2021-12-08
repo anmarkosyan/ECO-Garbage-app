9c32394a79ee055d4bf28a00d0e669d43cde9baf
Karen Boyakhchyan <karenboyakhchyan20@gmail.com>
2021-12-08 20:22:49 +0400

    Bug fixed

---
+++ src/entities/Services.ts
@@ -0,0 +1,21 @@
+import {
+    Entity,
+    Column,
+    OneToMany,
+    BaseEntity,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('service')
+export class ServiceEntity extends BaseEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id?: string;
+
+    @Column('varchar', { length: 100 })
+    type?: string;
+
+    @CreateDateColumn()
+    created_at?: Date;
+}
