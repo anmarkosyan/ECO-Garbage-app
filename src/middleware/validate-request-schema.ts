1ccb19363e7bdac88957e15cabba31267f5c9a8a
Anush Markosyan <Anush_Markosyan@epam.com>
2021-12-12 02:17:50 +0400

    added validate request file

--- src/middleware/validate-request-schema.ts
+++ src/middleware/validate-request-schema.ts
@@ -8,7 +8,7 @@ export function validateRequestSchema(
 ) {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({ errors: errors.array()[0] });
   }
   next();
 }
