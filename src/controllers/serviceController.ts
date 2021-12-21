643c2e52ac7066d43959af57a2b449ce6c4caf82
Anush Markosyan <Anush_Markosyan@epam.com>
2021-12-21 16:39:32 +0400

    some changes

--- src/controllers/serviceController.ts
+++ src/controllers/serviceController.ts
@@ -1,77 +1,85 @@
-import { Request, Response, NextFunction } from 'express';
-import { ServiceRepository } from '../services/service';
-import { HttpErr } from '../exceptions/HttpError';
+import {Request, Response, NextFunction} from 'express';
+import {ServiceRepository} from '../services/service';
+import {HttpErr} from '../exceptions/HttpError';
 import ExceptionMessages from '../exceptions/messages';
 import StatusCode from '../exceptions/statusCodes';
 
+export function add(a: number, b: number): number | null {
+    if (a >= 10 || b >= 10) {
+       // return null;
+        throw new Error('input number should be more than 10');
+    }
+    return a + b;
+}
+
 export class ServiceController {
-  static async createService(req: Request, res: Response, next: NextFunction) {
-    try {
-      const newService = req.body;
-      const sendToServiceData = await ServiceRepository.createService(
-        newService
-      );
-      res.status(StatusCode.CreateRequest).json(sendToServiceData);
-    } catch {
-      next(HttpErr.internalServerError(ExceptionMessages.INTERNAL));
+    static async createService(req: Request, res: Response, next: NextFunction) {
+        try {
+            const newService = req.body;
+            const sendToServiceData = await ServiceRepository.createService(
+                newService
+            );
+            res.status(StatusCode.CreateRequest).json(sendToServiceData);
+        } catch {
+            next(HttpErr.internalServerError(ExceptionMessages.INTERNAL));
+        }
     }
-  }
 
-  static async getAllServices(req: Request, res: Response, next: NextFunction) {
-    try {
-      const sendToServiceData = await ServiceRepository.getAllServices();
-      return res.status(200).json(sendToServiceData);
-    } catch {
-      next(HttpErr.internalServerError(ExceptionMessages.INTERNAL));
+    static async getAllServices(req: Request, res: Response, next: NextFunction) {
+        try {
+            const sendToServiceData = await ServiceRepository.getAllServices();
+            return res.status(200).json(sendToServiceData);
+        } catch {
+            next(HttpErr.internalServerError(ExceptionMessages.INTERNAL));
+        }
     }
-  }
 
-  static async getService(req: Request, res: Response, next: NextFunction) {
-    try {
-      const serviceId = req.params.id;
-      const sendToServiceData = await ServiceRepository.getService(serviceId);
-      if (!sendToServiceData) {
-        return next(HttpErr.notFound(ExceptionMessages.NOT_FOUND.SERVICE));
-      }
-      res.status(StatusCode.SuccessRequest).json(sendToServiceData);
-    } catch {
-      next(HttpErr.internalServerError(ExceptionMessages.INTERNAL));
+    static async getService(req: Request, res: Response, next: NextFunction) {
+        try {
+            const serviceId = req.params.id;
+            const sendToServiceData = await ServiceRepository.getService(serviceId);
+            if (!sendToServiceData) {
+                return next(HttpErr.notFound(ExceptionMessages.NOT_FOUND.SERVICE));
+            }
+            res.status(StatusCode.SuccessRequest).json(sendToServiceData);
+        } catch {
+            next(HttpErr.internalServerError(ExceptionMessages.INTERNAL));
+        }
     }
-  }
 
-  static async updateService(req: Request, res: Response, next: NextFunction) {
-    try {
-      const serviceId = req.params.id;
-      const newService = req.body;
+    static async updateService(req: Request, res: Response, next: NextFunction) {
+        try {
+            const serviceId = req.params.id;
+            const newService = req.body;
 
-      const updateData = await ServiceRepository.updateService(
-        serviceId,
-        newService
-      );
-      if (!updateData) {
-        return next(HttpErr.notFound(ExceptionMessages.NOT_FOUND.SERVICE));
-      }
-      res.status(StatusCode.SuccessRequest).json(updateData);
-    } catch {
-      next(HttpErr.internalServerError(ExceptionMessages.INTERNAL));
+            const updateData = await ServiceRepository.updateService(
+                serviceId,
+                newService
+            );
+            if (!updateData) {
+                return next(HttpErr.notFound(ExceptionMessages.NOT_FOUND.SERVICE));
+            }
+            res.status(StatusCode.SuccessRequest).json(updateData);
+        } catch {
+            next(HttpErr.internalServerError(ExceptionMessages.INTERNAL));
+        }
     }
-  }
 
-  static async deleteService(req: Request, res: Response, next: NextFunction) {
-    try {
-      const serviceId = req.params.id;
+    static async deleteService(req: Request, res: Response, next: NextFunction) {
+        try {
+            const serviceId = req.params.id;
 
-      const sendToServiceData = await ServiceRepository.deleteService(
-        serviceId
-      );
-      if (!sendToServiceData) {
-        return next(HttpErr.notFound(ExceptionMessages.NOT_FOUND.SERVICE));
-      }
-      res.status(200).json({
-        message: 'Service successfully deleted.',
-      });
-    } catch {
-      next(HttpErr.internalServerError(ExceptionMessages.INTERNAL));
+            const sendToServiceData = await ServiceRepository.deleteService(
+                serviceId
+            );
+            if (!sendToServiceData) {
+                return next(HttpErr.notFound(ExceptionMessages.NOT_FOUND.SERVICE));
+            }
+            res.status(200).json({
+                message: 'Service successfully deleted.',
+            });
+        } catch {
+            next(HttpErr.internalServerError(ExceptionMessages.INTERNAL));
+        }
     }
-  }
 }
